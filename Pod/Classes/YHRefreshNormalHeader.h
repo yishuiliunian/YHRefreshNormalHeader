47fecbd9920cca827c3e2cb8e1d58190b1bfb051
stonedong <yishuiliunian@gmail.com>
2016-10-24 10:50:00 +0800

    no message

---
+++ Pod/Classes/YHRefreshNormalHeader.h
@@ -0,0 +1,13 @@
+//
+//  YHRefreshNormalHeader.h
+//  YaoHe
+//
+//  Created by stonedong on 16/7/23.
+//  Copyright © 2016年 stonedong. All rights reserved.
+//
+
+#import <MJRefresh/MJRefresh.h>
+
+@interface YHRefreshNormalHeader : MJRefreshNormalHeader
+
+@end
---
+++ Pod/Classes/YHRefreshNormalHeader.m
@@ -0,0 +1,18 @@
+//
+//  YHRefreshNormalHeader.m
+//  YaoHe
+//
+//  Created by stonedong on 16/7/23.
+//  Copyright © 2016年 stonedong. All rights reserved.
+//
+
+#import "YHRefreshNormalHeader.h"
+
+@implementation YHRefreshNormalHeader
+
+- (void)placeSubviews
+{
+    [super placeSubviews];
+    self.lastUpdatedTimeLabel.hidden = YES;
+}
+@end
